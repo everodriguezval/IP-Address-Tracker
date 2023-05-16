f2c56741a1b94b750394cfb832b7dccda3f2f309
everodriguezval <eve.rodriguezval@gmail.com>
2023-05-16 17:30:20 +0100

    desktop version complete and event listeners added

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
+const searchInput = document.getElementById('input-IP');
+const searchArrow = document.querySelector('.arrow-icon-search');
+
+searchInput.addEventListener("input", (e) => {
+    console.log(e.target.value)
+})
+searchArrow.addEventListener("click", () => {
+    console.log('Arrow clicked')
+})
+
 // initialize the map and set its view to our chosen geographical coordinates and a zoom level
 var map = L.map('map').setView([51.505, -0.09], 13);
 
+// add a tile layer to add to our map
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+var marker = L.marker([51.5, -0.09]).addTo(map);
